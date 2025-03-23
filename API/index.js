164d2db3e0f90019c0eb2215c75578b780e1b0b9
SOURAV-RK <souravrk25@gmail.com>
2025-03-23 16:40:53 +0530

    Revert "revert 2"
    This reverts commit 42c2575040490c067f4c6bfe5f92d88e791e0404.

--- API/index.js
+++ API/index.js
@@ -1,13 +1,15 @@
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-const app = express();
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
 //fetch demo
 //login form
 //this is a test to study the branch
 //this is the second test
 
-app.listen(process.env.PORT, console.log("running on server 6000"));
+
+
+app.listen(process.env.PORT,console.log('running on server 6000') );
 //cherry pick
 //staged
 //demo diff
@@ -16,7 +18,5 @@ app.listen(process.env.PORT, console.log("running on server 6000"));
 
 //head1
 
-console.log("head1");
-console.log("head1 2");
-
-//revert 2
+console.log("head1")
+console.log("head1 2")
